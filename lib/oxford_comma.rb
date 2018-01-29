a003da2759c1de1d82bb7bbee5efabcb3beac305
Harleigh Abel <harleighabel@gmail.com>
2018-01-29 18:29:04 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,7 @@
 def oxford_comma(array)
-  if array.length <= 1
-    array.join
-  elsif array.length == 2
-    string = array.join (" and ")
-  else
-  last_word = array.pop
-  string = array.join (", ")
-  string << ", and " + last_word
-  end
-end
+  last_word = array.last
+  array.pop
+  string = array.split (", ")
+  string = string + "and #{last_word}."
+  string
+end  
