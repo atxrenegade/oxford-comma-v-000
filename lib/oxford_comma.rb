08bf0502907172539865557c0e57d56ebd0408d8
Harleigh Abel <harleighabel@gmail.com>
2018-01-29 18:31:36 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,6 @@ def oxford_comma(array)
   last_word = array.last
   array.pop
   string = array.join(", ")
-  string = string + ", and #{last_word}."
+  string = string + ", and #{last_word}"
   string
 end
